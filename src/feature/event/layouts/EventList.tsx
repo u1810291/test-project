827dd191abc2bc70d94fa1707a97e28c2402902a
VRIZAOT <otabekbutcher@gmail.com>
2022-09-25 18:50:03 +0500

    fix: fixing ui representation of components

--- src/feature/event/layouts/EventList.tsx
+++ src/feature/event/layouts/EventList.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import { Button } from '../../../components/Button';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
 import {ReactComponent as CreateIMG} from '../../../assets/create-event.svg';
-import { Item, useStyles } from './style'
+import { useStyles } from './style'
 
 export const EventList = () => {
   const classes = useStyles();
   return(
     <Box className={classes.root} sx={{ flexGrow: 1 }}>
-      <Grid className={classes.grid} container spacing={2}>
-        <Grid item xs={6}>
-          <Item>
-            <CreateIMG />
-          </Item>
-        </Grid>
-        <Grid item xs={6}>
-          <Item>
-            <Button title='🎉 Create my event'/>
-          </Item>
-        </Grid>
-      </Grid>
+      <Box className={classes.grid}>
+        <Box className={classes.leftSide}>
+          <CreateIMG />
+        </Box>
+        <Box className={classes.topRight}>
+          Imagine if had events.
+        </Box>
+        <Box className={classes.bottomRight}>
+          <Button title='🎉 Create my event'/>
+        </Box>
+      </Box>
     </Box>
   )
 }
--- src/feature/event/layouts/style.ts
+++ src/feature/event/layouts/style.ts
@@ -22,5 +22,35 @@ export const useStyles = makeStyles({
   },
   grid: {
     height: '100vh',
-  }
+    width: '100vw',
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gap: '0px 0px',
+    gridTemplateAreas: `"leftSide top-right ."
+      "leftSide bottom-right ."`,
+    gridGap: '10px',
+  },
+  leftSide: {
+    gridArea: 'leftSide',
+    display: 'flex',
+    alignItems: 'center',
+    width: '100%',
+    justifyContent: 'center',
+  },
+  topRight: {
+    gridArea: 'top-right',
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'end'
+  },
+  bottomRight: {
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+    alignItems: 'start',
+    justifyContent: 'center',
+    gridArea: 'bottom-right',
+  },
 })
